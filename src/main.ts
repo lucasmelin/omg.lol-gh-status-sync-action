6e179d62259b8b8b52eac1c4618d90292b74664b
Lucas Melin <lucas.melin@gmail.com>
2023-01-22 21:17:45 -0500

    Regenerate package-lock
    npm install --package-lock

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import * as core from '@actions/core'
-import { fetchStatus } from './fetch-status'
-import { updateStatus } from './update-status'
+import {fetchStatus} from './fetch-status'
+import {updateStatus} from './update-status'
 
 async function run(): Promise<void> {
   try {
     const omgLolUser = core.getInput('omg lol user')
-    const { emoji, message } = await fetchStatus(
+    const {emoji, message} = await fetchStatus(
       `https://api.omg.lol/address/${omgLolUser}/statuses/latest`
     )
-    core.setOutput("emoji", emoji)
-    core.setOutput("message", message)
+    core.setOutput('emoji', emoji)
+    core.setOutput('message', message)
     const userToken = core.getInput('github token')
     await updateStatus(userToken, emoji, message)
   } catch (error) {
